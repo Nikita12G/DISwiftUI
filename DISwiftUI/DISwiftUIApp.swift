8b123d01f45a017131a7c1f49cfec68b86c50f30
Nikita12G <gulyi08@mail.ru>
2024-08-20 11:14:14 +0700

    Initial Commit

---
+++ DISwiftUI/DISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DISwiftUIApp.swift
+//  DISwiftUI
+//
+//  Created by Никита Гуляев on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
